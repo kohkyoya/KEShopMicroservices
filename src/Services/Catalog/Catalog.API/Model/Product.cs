3fbd0242278ac6d1c3b42f3d1b72b354a5fc3b42
koh <gogosi072@gmail.com>
2024-07-08 23:13:42 +0700

    feat : create Product model.

---
+++ src/Services/Catalog/Catalog.API/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.API.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public List<string> Categories { get; set; } = new List<string>();
+        public string Description { get; set; } = string.Empty;
+        public string ImageFile { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
